fc8505acc9869e99a878649c0f857487122022fc
TrialNTerror <99099246+TrialNTerror@users.noreply.github.com>
2023-11-01 17:55:31 -0500

    PID changed and PathPlanner rotating
    PID numbers are better. Rotating in auto with PathPlanner doesn't work, I don't know how to fix :(

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -28,7 +28,7 @@ public final class Constants
   {
 
     public static final PIDFConfig xAutoPID     = new PIDFConfig(0.7, 0, 0);
-    public static final PIDFConfig yAutoPID     = new PIDFConfig(0.7, 0, 0);
+    public static final PIDFConfig yAutoPID     = new PIDFConfig(1.6, 0, 0);
     public static final PIDFConfig angleAutoPID = new PIDFConfig(0.4, 0, 0.01);
 
     public static final double MAX_SPEED        = 4;
